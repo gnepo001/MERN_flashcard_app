9a523819fc0929010be34f22b6a6f4ff682dd82b
Gilbert Nepomuceno <gilbertnepomuceno99@gmail.com>
2022-01-23 22:21:42 -0800

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { AddCardForm } from "./components/index.js";
+
+const App = () => {
+  return (
+    <div>
+      <h1>test</h1>
+      <AddCardForm />
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/src/components/AddCardForm/AddCardForm.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Paper from "@mui/material/Paper";
+
+const AddCardForm = () => {
+  return (
+    <>
+      <h1>Form</h1>
+    </>
+  );
+};
+
+export default AddCardForm;
