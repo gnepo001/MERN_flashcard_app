a461329f3fccee69a063248e232d2a6e33fc1cce
Gilbert Nepomuceno <gilbertnepomuceno99@gmail.com>
2022-03-18 23:02:18 -0700

    adding update Func

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -32,3 +32,16 @@ export const deletePost = async (req, res) => {
   await flashcard.findByIdAndRemove(_id);
   res.json({ message: "Post deleted successfully" });
 };
+
+export const updatePost = async (req, res) => {
+  const { id: _id } = req.params;
+  const post = req.body;
+  if (!mongoose.Types.ObjectId.isValid(_id))
+    return res.status(404).send("No post with that id");
+  const updatePost = await flashcard.findByIdAndUpdate(
+    _id,
+    { ...post, _id },
+    { new: true }
+  );
+  res.json(updatePost);
+};
--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { createPost, getPosts, deletePost } from "../controllers/post.js";
+import {
+  createPost,
+  getPosts,
+  deletePost,
+  updatePost,
+} from "../controllers/post.js";
 
 const router = express.Router();
 
 router.post("/", createPost);
 router.get("/", getPosts);
 router.delete("/:id", deletePost);
+router.patch("/:id", updatePost);
 
 export default router;
