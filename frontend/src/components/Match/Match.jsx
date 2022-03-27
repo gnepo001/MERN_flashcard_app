341f867fb9437d0587448352f6da6f6be0393940
Gilbert Nepomuceno <gilbertnepomuceno99@gmail.com>
2022-03-27 15:45:38 -0700

    randomization of defs for match game

--- frontend/src/components/Match/Match.jsx
+++ frontend/src/components/Match/Match.jsx
@@ -8,19 +8,58 @@ import useStyles from "./styles";
 const Match = () => {
   const classes = useStyles();
   const items = useSelector((state) => state.posts);
+
+  let x = null;
+
+  const setId = (id) => {
+    x = id;
+  };
+
+  const checkId = (id) => {
+    if (id === x) {
+      console.log("Match");
+      console.log(`${id} : ${x}`);
+    } else {
+      console.log("no match");
+      console.log(`${id} : ${x}`);
+    }
+  };
+
+  const arrs = [];
+  const arrs2 = [];
+  const arrs3 = [];
+  for (let i = 0; i < items.length; i++) {
+    let temp = items[i]._id;
+
+    arrs.push({ _id: temp, word: items[i].word });
+    arrs2.push({ _id: temp, definition: items[i].definition });
+  }
+  for (let i = 0; i < items.length; i++) {
+    let temp = items[i]._id;
+    let temp2 = arrs2[Math.floor(Math.random() * arrs2.length)];
+    console.log(temp2);
+    arrs3.push({ _id: temp, definition: temp2.definition });
+  }
+
+  //items.map((item) => arrs.push({item._id:"test"})); does not Work
+
   return (
     <div>
       <Grid container spacing={2}>
-        {items.map((item) => (
-          <Fragment key={item._id}>
+        {arrs.map((ar) => (
+          <Fragment key={`word${ar._id}`}>
             <Grid item xs={6}>
-              <Button fullWidth>
-                <SimpleCard item={item.word}></SimpleCard>
+              <Button fullWidth onClick={() => console.log("tt")}>
+                <SimpleCard item={ar.word}></SimpleCard>
               </Button>
             </Grid>
+          </Fragment>
+        ))}
+        {arrs3.map((arr) => (
+          <Fragment key={`def${arr._id}`}>
             <Grid item xs={6}>
-              <Button fullWidth>
-                <SimpleCard item={item.definition}></SimpleCard>
+              <Button fullWidth onClick={() => console.log("pressed")}>
+                <SimpleCard item={arr.definition}></SimpleCard>
               </Button>
             </Grid>
           </Fragment>
