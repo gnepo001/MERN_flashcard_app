99a552270d784d1a02665aa5ac0573de88f6a130
Gilbert Nepomuceno <gilbertnepomuceno99@gmail.com>
2022-03-28 09:45:46 -0700

    code cleanUp

--- frontend/src/components/Match/Match.jsx
+++ frontend/src/components/Match/Match.jsx
@@ -10,65 +10,47 @@ const Match = () => {
   const items = useSelector((state) => state.posts);
 
   let x = null;
-
-  const setId = (id) => {
-    x = id;
-    console.log(`SetID: ${id}`);
-  };
+  const wordArr = [];
+  const randomArr = [];
 
   const checkId = (id) => {
-    console.log(`CheckID: ${id}`);
     if (x) {
       if (id === x) {
         console.log("Match");
-        console.log(`${x} : ${id}`);
       } else {
         console.log("no match");
-        console.log(`${x} : ${id}`);
       }
       x = null;
     } else {
-      setId(id);
-      console.log(`SetID: ${id}`);
+      x = id;
     }
   };
 
-  const arrs = [];
-  const arrs2 = [];
-  const arrs3 = [];
+  //need to split up items objs to two seprate arrs in order
+  //to randomize defs for the matching game
   for (let i = 0; i < items.length; i++) {
-    let temp = items[i]._id;
-
-    arrs.push({ _id: temp, word: items[i].word });
-    arrs2.push({ _id: temp, definition: items[i].definition });
+    wordArr.push({ _id: items[i]._id, definition: items[i].definition });
   }
-
   for (let i = 0; i < items.length; i++) {
-    let temp = items[i]._id;
-    let temp2 = arrs2[Math.floor(Math.random() * arrs2.length)];
-    let temp3 = arrs2.indexOf(temp2);
-    //console.log(`${temp2._id} : ${temp2.definition}`);
-    arrs3.push({ _id: temp2._id, definition: temp2.definition }); //error here- the def has the same id as the word this needs to be fixed
-    arrs2.splice(temp3, 1);
-    //console.log(arrs3);
-    //console.log(`Loop2: ${temp}, ${arrs3[1]._id}`);
+    let temp2 = wordArr[Math.floor(Math.random() * wordArr.length)]; //gets random obj
+
+    randomArr.push({ _id: temp2._id, definition: temp2.definition });
+    wordArr.splice(wordArr.indexOf(temp2), 1); //deletes random obj so does not get called again
   }
 
   return (
     <div>
       <Grid container spacing={2}>
-        {arrs.map((ar, i) => (
+        {items.map((ar, i) => (
           <Fragment key={`word${ar._id}`}>
             <Grid item xs={6}>
               <Button fullWidth onClick={() => checkId(ar._id)}>
-                {console.log(ar._id)}
                 <SimpleCard item={items[i].word}></SimpleCard>
               </Button>
             </Grid>
             <Grid item xs={6}>
-              <Button fullWidth onClick={() => checkId(arrs3[i]._id)}>
-                {console.log(arrs3[i]._id)}
-                <SimpleCard item={arrs3[i].definition}></SimpleCard>
+              <Button fullWidth onClick={() => checkId(randomArr[i]._id)}>
+                <SimpleCard item={randomArr[i].definition}></SimpleCard>
               </Button>
             </Grid>
           </Fragment>
